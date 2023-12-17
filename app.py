43713ae82a9633dae9eb4d7246c23d5bf9e4ba1d
Prakarsh Sharma <prakarshs@sigmoidanalytics.com>
2023-12-17 19:01:03 +0530

    built an app to parse natural language as JSON

--- app.py
+++ app.py
@@ -1,50 +1,47 @@
 import streamlit as st
 import openai
-from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, llms, memory
+client = openai.OpenAI(api_key=st.secrets.openai_key)
 
-openai.api_key = st.secrets.openai_key
 st.header("Chat with a Revenue Growth Manager 💬 📚")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
     st.session_state.messages = [
                                 {"role": "assistant", 
-                                 "content": "Ask me a question RGM for your CPG company..."}
+                                 "content": "Ask me a question about RGM for your CPG company..."}
                                 ]
 
-@st.cache_resource(show_spinner=False)
-def load_and_index():
-    sys_prom = """You are an expert on Revenue Growth Management (RGM).
-    Your job is to answer questions on RGM.
-    Assume that all questions are related to RGM.
-    Answers based on facts only."""
-    llm = llms.OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt=sys_prom)
-    with st.spinner(text="Loading and indexing the documentation..."):
-        reader = SimpleDirectoryReader(input_dir="./docs", recursive=True)
-        docs = reader.load_data()
-        sc = ServiceContext.from_defaults(llm=llm)
-        index = VectorStoreIndex.from_documents(docs, service_context=sc)
-        return index
+with open("examples.txt", "r") as f:
+    examples = f.read()
 
-index = load_and_index()
+def system_prompt_template(examples):
+    prompt = f"""consider the examples delimited with triple backticks and\
+        answer the followeing question in the same manner:
+        ```
+        {examples}
+        ```
+        """
+    return prompt
 
-mode = "condense_question"
-# mode = "context"
-max_token = 1500
-mem = memory.ChatMemoryBuffer.from_defaults(token_limit=1500)
-chat_engine = index.as_chat_engine(chat_mode=mode, memory=mem, verbose=True)
+# st.session_state.messages.append({"role": "system", "content": system_prompt_template(examples)})
 
-prompt = st.chat_input("Your question...")
-if prompt: # prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+question = st.chat_input("Your question...")
+if question: # prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": question})
 
 for message in st.session_state.messages: # display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-if st.session_state.messages[-1]["role"] != "assistant":
+if st.session_state.messages[-1]["role"] == "user":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
+            prompt = system_prompt_template(examples) + "\n\n" + f"Question: {question}" + "\n" + "Answer: "
+            response = client.completions.create(model='gpt-3.5-turbo-instruct', 
+                                                 prompt=prompt, 
+                                                 temperature=0, 
+                                                 max_tokens=100)
+            response = response.choices[0].text
+            st.write(response)
+            message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message) # add response to message history
+            # examples += "\n\n" + question + "\n" + response
